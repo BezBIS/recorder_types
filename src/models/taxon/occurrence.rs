7ab339e3b87f0ccc10fd24f0f9166445dd0375e2
BezBIS <berwyn@bis.org.uk>
2022-09-07 11:46:27 +0100

    Generate getter methods

--- src/models/taxon/occurrence.rs
+++ src/models/taxon/occurrence.rs
@@ -24,3 +24,77 @@ pub struct TaxonOccurrence {
     changed_date: Option<DateTime<Utc>>,
     custodian: Option<String>,
 }
+
+impl TaxonOccurrence {
+    pub fn key(&self) -> &str {
+        &self.taxon_occurrence_key
+    }
+
+    pub fn comment(&self) -> Option<&String> {
+        self.comment.as_ref()
+    }
+
+    pub fn zero_abundance(&self) -> bool {
+        self.zero_abundance
+    }
+
+    pub fn confidential(&self) -> bool {
+        self.confidential
+    }
+
+    pub fn verified(&self) -> i16 {
+        self.verified
+    }
+
+    pub fn checked(&self) -> bool {
+        self.checked
+    }
+
+    pub fn checked_by(&self) -> Option<&String> {
+        self.checked_by.as_ref()
+    }
+
+    pub fn checked_date(&self) -> Option<DateTime<Utc>> {
+        self.checked_date
+    }
+
+    pub fn surveyors_ref(&self) -> Option<&String> {
+        self.surveyors_ref.as_ref()
+    }
+
+    pub fn provenance(&self) -> Option<&String> {
+        self.provenance.as_ref()
+    }
+
+    pub fn sample_key(&self) -> &str {
+        self.sample_key.as_ref()
+    }
+
+    pub fn substrate_key(&self) -> Option<&String> {
+        self.substrate_key.as_ref()
+    }
+
+    pub fn record_type_key(&self) -> Option<&String> {
+        self.record_type_key.as_ref()
+    }
+
+    pub fn entered_by(&self) -> &str {
+        self.entered_by.as_ref()
+    }
+
+    pub fn entry_date(&self) -> DateTime<Utc> {
+        self.entry_date
+    }
+
+    pub fn changed_by(&self) -> Option<&String> {
+        self.changed_by.as_ref()
+    }
+
+    pub fn changed_date(&self) -> Option<DateTime<Utc>> {
+        self.changed_date
+    }
+
+    pub fn custodian(&self) -> Option<&String> {
+        self.custodian.as_ref()
+    }
+}
